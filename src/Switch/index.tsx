4e9086b9cae1bd1017aef0488112d2228cf23bb9
Januar Asprilla <ajanuarandres@gmail.com>
2024-02-05 18:32:12 -0500

    feat(Switch): implementation of Switch component

---
+++ src/Switch/index.tsx
@@ -0,0 +1,75 @@
+import { MdDone, MdClose } from "react-icons/md";
+
+import { Stack } from "@inubekit/stack";
+import { Label } from "@inubekit/label";
+
+import { StyledContainer, StyledInput, StyledSpan, StyledIcon } from "./styles";
+import { Size } from "./props";
+
+interface ISwitchProps {
+  id: string;
+  name?: string;
+  value?: string;
+  size?: Size;
+  checked?: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  label: string;
+  margin: string;
+  padding: string;
+  disabled?: boolean;
+}
+
+export const Switch = (props: ISwitchProps) => {
+  const {
+    disabled = false,
+    id,
+    name,
+    value,
+    size = "small",
+    checked = false,
+    onChange,
+    label,
+    margin = "s0",
+    padding = "s0",
+  } = props;
+
+  return (
+    <Stack
+      direction={"row"}
+      justifyContent={label ? "space-between" : "flex-start"}
+      alignItems="center"
+      gap={label ? "10px" : "0px"}
+      margin={margin}
+      padding={padding}
+    >
+      <StyledContainer $size={size}>
+        <StyledInput
+          $id={id}
+          $type="checkbox"
+          $size={size}
+          $value={value}
+          $checked={checked}
+          $onChange={onChange}
+          $disabled={disabled}
+          $name={name}
+        />
+        <StyledSpan $size={size} $disabled={disabled} $checked={checked}>
+          {checked ? (
+            <StyledIcon $checked={checked} $size={size} $disabled={disabled}>
+              <MdDone id="mdIcon" />
+            </StyledIcon>
+          ) : (
+            <StyledIcon $checked={checked} $size={size} $disabled={disabled}>
+              <MdClose id="mdIcon" />
+            </StyledIcon>
+          )}
+        </StyledSpan>
+      </StyledContainer>
+      {label && (
+        <Label htmlFor={id} disabled={disabled}>
+          {label}
+        </Label>
+      )}
+    </Stack>
+  );
+};
---
+++ src/Switch/stories/Switch.stories.tsx
@@ -0,0 +1,47 @@
+import { Switch, ISwitchProps } from "..";
+import { SwitchController } from "./SwitchController";
+import { ThemeProvider } from "styled-components";
+
+import { presente } from "@inubekit/foundations";
+import { props, parameters } from "../props";
+
+const story = {
+  title: "inputs/Switch",
+  components: [Switch],
+  parameters,
+  argTypes: {
+    ...props,
+    onchange: { action: "onChange" },
+  },
+};
+
+export const Default = (args: ISwitchProps) => (
+  <SwitchController {...args} onSwitchChange={args.onChange} />
+);
+Default.args = {
+  id: "id",
+  disabled: false,
+  name: "name",
+  value: "switchTest1",
+  checked: false,
+  size: "small",
+  onChange: () => {},
+  margin: "s0",
+  padding: "s0",
+};
+
+const theme = {
+  ...presente,
+};
+
+export const Themed = (args: ISwitchProps) => (
+  <ThemeProvider theme={theme}>
+    <SwitchController {...args} />
+  </ThemeProvider>
+);
+
+Themed.args = {
+  ...Default.args,
+};
+
+export default story;
