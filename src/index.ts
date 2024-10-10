aa476b0d6d48385950e3c18dd8d010ce2032500f
Cesar Marin Alfonso <45011420+cmarin001@users.noreply.github.com>
2024-10-10 11:46:04 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Toggle } from "./Toggle";
 export type { IToggle } from "./Toggle";
 export type { IToggleSize } from "./Toggle/props";
+export { tokens as ToggleTokens } from "./Toggle/Tokens/tokens";
